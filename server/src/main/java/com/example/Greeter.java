8af928e5acf4aad0c32697b27190173bad483a1e
krishna-sutar <45293784+krishna-sutar@users.noreply.github.com>
2019-03-12 20:51:04 +0530

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -8,12 +8,12 @@ public class Greeter {
   /**
    * This is a constructor.
    */
-  public Greeter() {
-
+  public Greeter() 
+  {
   }  
-
   //TODO: Add javadoc comment
-  public String greet(String someone) {
+  public String greet(String someone) 
+  {
     return String.format("Hello, %s!", someone);
   }
 }
